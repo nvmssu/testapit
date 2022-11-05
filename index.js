866e7644819cb7269f50f5e76d2b66d434202fd8
vet <ngovanthitruyen@gmail.com>
2022-11-05 23:13:45 +0700

    testapp

---
+++ index.js
@@ -0,0 +1,26 @@
+// var unde = require("underscore");
+// let n = [3, 4, 6, 7, 8, 23, 2];
+// let max = unde.max(n);
+// console.log(45353);
+// let mapp = unde.min(n);
+// console.log(mapp);
+// let gg = unde.map(n);
+// console.log(gg);
+// let dsf = unde.random(n) * 5;
+// console.log(dsf);
+// const https = require("https");
+// const fetch = require("node-fetch");
+// // const fetch1 = require("node-fetch@2.6.1");
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//   // const axios = require("axios");
+//   // axio("http://example.com/movies.json")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+console.log(heloo("vet"));
+console.log(`${heloo.name}`);
+console.log(typeof heloo);
+
+function heloo(name) {
+  return `xin chao ${name}`;
+}
